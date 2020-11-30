419c297edefa9d3b563da39abb45e5bfcc4702cd
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-11-30 15:02:50 -0300

    routes cocktails controller index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see https://guides.rubyonrails.org/routing.html
+  # resources :cocktails, only: [:index, :show, :create] do
+  #   resources :
+  # end
+  get '/cocktails', to: 'cocktails#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,15 @@ puts "Adding ingredients..."
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "Tomato Juice")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Pina Colada")
 
 p Ingredient.all
